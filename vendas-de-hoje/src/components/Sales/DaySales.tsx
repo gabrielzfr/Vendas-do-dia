b67659d0dcb42d2b2d5c806d523b703a82cb60db
Gabriel Francis <69304692+gabrielzfr@users.noreply.github.com>
2023-01-25 22:35:00 -0300

    Update DaySales.tsx

--- vendas-de-hoje/src/components/Sales/DaySales.tsx
+++ vendas-de-hoje/src/components/Sales/DaySales.tsx
@@ -19,10 +19,10 @@ export function DaySales(props: DaySalesProps) {
 
     return (
         <section className="flex flex-col items-center gap-6">
-        <h1 className="sm:text-7xl text-5xl font-semibold">
+        <h1 className="sm:text-7xl text-5xl font-semibold text-center">
             Vendas do Dia
         </h1>
-        <article className="md:w-[48rem] h-[42rem] bg-grayBg  sm:px-10 px-5 py-6 overflow-y-auto w-[95%]">
+        <article className="md:w-[48rem] h-[42rem] bg-grayBg sm:px-10 px-5 py-6 overflow-y-auto w-[95vw]">
             <ul className="w-full flex sm:gap-8 gap-5 flex-wrap justify-center">
             {props.sales.map((sale, index) => {
                     return (
@@ -38,7 +38,7 @@ export function DaySales(props: DaySalesProps) {
             <input 
             type="button" 
             value="Gerar Relátorio"
-            className="bg-aquaBlue sm:w-[32rem] h-[5rem] text-4xl cursor-pointer hover:opacity-80 transition-opacity w-[95%] "
+            className="bg-aquaBlue sm:w-[32rem] h-[5rem] text-4xl cursor-pointer hover:opacity-80 transition-opacity w-[95vw] "
             onClick={() => props.setShowReport(true)}
             />
     </section>
