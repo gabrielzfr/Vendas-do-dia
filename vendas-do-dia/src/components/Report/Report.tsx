d7e060e7a842de2104a11eaa50f81fa701abf083
Gabriel Francis <69304692+gabrielzfr@users.noreply.github.com>
2023-03-13 15:45:32 -0300

    aq

--- vendas-do-dia/src/components/Report/Report.tsx
+++ vendas-do-dia/src/components/Report/Report.tsx
@@ -9,7 +9,7 @@ import { useSetShowComponents } from "../../common/state/hooks/useSetShowCompone
 export function Report() {
   const [takingPrint, isTakingPrint] = useState(false);
 
-  const [copyImageError, handleCopyImageError] = useState<null | boolean>(false);
+  const [copyImageError, handleCopyImageError] = useState<null | boolean>(null);
 
   const { creditSalesTotal, moneySalesTotal, pixSalesTotal, totalSales } = useTotalSalesSelector()
 
